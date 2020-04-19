96397da465ee1d4083d3344e729fa4e86a7187bf
Jon Choi <choi.jonathanh@gmail.com>
2020-04-18 22:02:38 -0700

    show route for user controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   get 'home/index'
 
   root to: 'home#index'
+
+  # http://localhost:3000/users/:id to the user controller's show action,
+  # with :id is ID of current user. If ID of current user is 1, path point
+  # to user profile page is http://localhost:3000/users/1
+  resources :users, only: [:show]
 end
