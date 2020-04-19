e91303d8b6391b9541dab5b7d5258a6891afb351
Jon Choi <choi.jonathanh@gmail.com>
2020-04-19 13:56:16 -0700

    edit profile functionality

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   # http://localhost:3000/users/:id to the user controller's show action,
   # with :id is ID of current user. If ID of current user is 1, path point
   # to user profile page is http://localhost:3000/users/1
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
 end
